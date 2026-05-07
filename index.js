b1fa07c1d68fb4088befd1316e8a92c1229becea
Cejhle <lerchy555@gmail.com>
2026-05-07 13:55:14 +0700

    update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express()
 
 app.use(express.static('public'));
 
-// app.get("/", (req, res) => {
-//   const file = path.join(process.cwd(), "public", "index.html")
-//   res.sendFile(file)
-// })
+app.get("/", (req, res) => {
+  const file = path.join(process.cwd(), "public", "index.html")
+  res.sendFile(file)
+})
 
 
 export default app;
