0d8113ba2b5c57148c85c4e1ec88822bfc61ee05
Cejhle <lerchy555@gmail.com>
2026-05-07 13:51:52 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from "express"
 import path from "path"
 const app = express()
-const port = 7777
 
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  const file = path.join(process.cwd(), "public", "index.html")
-  res.sendFile(file)
-})
+// app.get("/", (req, res) => {
+//   const file = path.join(process.cwd(), "public", "index.html")
+//   res.sendFile(file)
+// })
 
 
 export default app;
